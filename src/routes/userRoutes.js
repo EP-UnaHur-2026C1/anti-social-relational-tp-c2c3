1bfaaeef93a69474bf9e08516d76329278670336
matiasnc <mn.calderon97@gmail.com>
2026-06-07 16:57:30 -0300

    Agrego mdw de createUser a la ruta de usuario

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,16 +4,15 @@ import { createUser,
     getUserByNickName,
     deleteUser
  } from '../controllers/userController.js';
+import { validateSchema, createUserSchema } from '../schemas/userSchema.js';
 
 const router = Router();
 
+router.post('/', validateSchema(createUserSchema), createUser);
+
 router.post('/', createUser);
-
 router.get('/', getAllUsers);
-
 router.get('/:nickName', getUserByNickName);
-
 router.delete('/:nickName', deleteUser);
-
 
 export default router;
